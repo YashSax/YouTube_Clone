c9067b2a8fa2d1adb30ba0aa97d9dc22aee97b7d
Yash Saxena <yash.saxena@gmail.com>
2024-02-26 21:12:45 -0600

    Started on videocard

---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+import { Typography, Card, CardContent, CardMedia } from '@mui/material'
+import { CheckCircle } from '@mui/icons-material'
+
+import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelTitle, demoChannelUrl} from '../utils/constants'
+
+const VideoCard = () => {
+  return (
+    <div>VideoCard</div>
+  )
+}
+
+export default VideoCard
